d0320a65e10d2c703a96de68505450eb1970efbf
tjklein23 <thomasklein23@augustana.edu>
2026-03-03 14:12:01 -0600

    Connected project to firebase

---
+++ FEP-tracker/src/firebase-config.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAsZIG9SxdtNaVHreR2ifKLcEGtA6wZ11g",
+  authDomain: "fep-tracker.firebaseapp.com",
+  projectId: "fep-tracker",
+  storageBucket: "fep-tracker.firebasestorage.app",
+  messagingSenderId: "330729366554",
+  appId: "1:330729366554:web:6188051b66988f56349da3",
+  measurementId: "G-22797BTXDV"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
